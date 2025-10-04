1dabd3d5c549fba52d45da341210dc65516a12b9
Hanna Tariku <hannatariku1996@gmail.com>
2025-10-04 19:59:27 +0300

    feat(views): add function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('', include('relationship_app.urls')),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
+    
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
 ]
