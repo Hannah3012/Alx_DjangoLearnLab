e193d6a19184ab0162cb496ec0f7857c16b6f326
Hanna Tariku <hannatariku1996@gmail.com>
2025-10-17 11:46:32 +0300

    customized CreateView and UpdateView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.permissions import BasePermission
+from rest_framework import status
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author__name']
     
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -14,11 +20,36 @@ class BookDetailView(generics.RetrieveAPIView):
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [permissions.IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get.serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status= status.HTTP_201_CREATED, headers=headers)
+    
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [permissions.IsAuthenticated]
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer) 
+        return Response(serializer.data)
+    
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.author.user == request.user 
+    
+    permissions.classes = [IsAuthorOrReadOnly]
