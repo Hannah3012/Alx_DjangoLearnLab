1222cc1c0d23d569b493eba93b141675795ccb3d
Hanna Tariku <hannatariku1996@gmail.com>
2025-10-17 11:00:43 +0300

    defined data models

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
-class Book(models.model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name='books')
